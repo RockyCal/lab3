cd813d2016c05640ecf9664243b905258aeee01c
Raquel <calderon.rocky@gmail.com>
2016-01-21 13:42:48 -0800

    togglable project descriptions

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,27 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Javascript has been tested.");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("div.project").click(projectClick);
+	$(".project-media").hide();
+}
+
+function projectClick(e) {
+	// prevent the page from reloading
+	e.preventDefault();
+
+	// In an event handler, $(this) refers to
+	// the object that triggered the event
+	var projectTitle = $(this).find("a").find("p.project-title").text();
+	var jumbotronHeader = $(".jumbotron h1");
+	jumbotronHeader.text(projectTitle);
+
+	var containingProject = $(this).closest(".project");
+	var media = $(containingProject).find(".project-media");
+	$(media).fadeToggle()
 }
